cf7970225152de79f11012dcc08c2474b6104bdb
Scortsh <andreasjnordin@gmail.com>
2025-03-03 04:12:22 +0100

    Reformatting with prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(<h1>Hello react</h1>)
+createRoot(document.getElementById("root")).render(<h1>Hello react</h1>);
